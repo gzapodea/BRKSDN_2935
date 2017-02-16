17b0dd90c50553b5c283d378afadb6d93cf7eb09
gzapodea <gzapodea@cisco.com>
2017-02-16 11:48:41 -0800

    ready for Cisco Live

--- BRKSDN_2935_ERNA.py
+++ BRKSDN_2935_ERNA.py
@@ -1,2 +1,50 @@
+# developed by Gabi Zapodeanu, TSA, GSS, Cisco Systems
 
+# !/usr/bin/env python3
 
+# !/usr/bin/env python3
+
+import requests
+import json
+import time
+import requests.packages.urllib3
+import base64
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+from requests.auth import HTTPBasicAuth  # for Basic Auth
+
+from ERNA_init import SPARK_AUTH
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)  # Disable insecure https warnings
+
+# The following declarations need to be updated based on your lab environment
+
+PI_URL = 'https://172.16.11.25'
+PI_USER = 'python'
+PI_PASSW = 'Clive.17'
+PI_AUTH = HTTPBasicAuth(PI_USER, PI_PASSW)
+
+EM_URL = 'https://172.16.11.30/api/v1'
+EM_USER = 'python'
+EM_PASSW = 'Clive.17'
+
+CMX_URL = 'https://172.16.11.27/'
+CMX_USER = 'python'
+CMX_PASSW = 'Clive!17'
+CMX_AUTH = HTTPBasicAuth(CMX_USER, CMX_PASSW)
+
+SPARK_URL = 'https://api.ciscospark.com/v1'
+
+ASAv_URL = 'https://172.16.11.5'
+ASAv_USER = 'python'
+ASAv_PASSW = 'cisco'
+ASAv_AUTH = HTTPBasicAuth(ASAv_USER, ASAv_PASSW)
+
+ASAv_CLIENT = '172.16.41.55'
+ASAv_REMOTE_CLIENT = '172.16.203.50'
+
+UCSD_URL = 'https://https://10.94.132.69'
+UCSD_USER = 'gzapodea'
+UCSD_PASSW = 'cisco.123'
+UCSD_KEY = '1D3FD49A0D474481AE7A4C6BD33EC82E'
+UCSD_CONNECT_FLOW = 'Gabi_VM_Connect_VLAN_10'
+UCSD_DISCONNECT_FLOW = 'Gabi_VM_Disconnect_VLAN_10'
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# BRKSDN_2935
+BRKSDN_2935
+
 This repository will be used for the Cisco Live Berlin  BRKSDN-2935 session.  
 
 Software included: ERNA.py - full lab/POC code.
